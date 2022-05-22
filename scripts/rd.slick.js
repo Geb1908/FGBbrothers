6aae2fde173cbc0b135af8781005dc6f7448c9ff
GEB <bermudez.e.gonzalo@gmail.com>
2022-05-22 17:34:14 -0300

    Se agregan los scripts

---
+++ scripts/rd.slick.js
@@ -0,0 +1,34 @@
+$(".responsive").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  prevArrow: '<i class="fas fa-angle-left left__arrow">',
+  nextArrow: '<i class="fas fa-angle-right right__arrow">',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
